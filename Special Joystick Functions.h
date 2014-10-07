49e5a5dbb84198bedf8a35c3e9ca667a52dfc51d
FTC Team 359 Gaulbots2 <nshupe@webb.org>
2014-10-07 15:06:33 -0700

    Added function
    Added Linear function, which incorporates deadzones and max speed to
    make sure that no control is wasted

--- Special Joystick Functions.h
+++ Special Joystick Functions.h
@@ -7,3 +7,12 @@ int joystickExponential(int deadzone, int maxSpeed, int joystickCurrent){
 	long newInside = maxSpeed-sqrt(((abs(joystickCurrent)-127)*maxSpeed2)/(deadzone-127));
 	return polarity*newInside;
 }
+
+int joystickLinear(int deadzone, int maxSpeed, int joystickCurrent){
+	if(joystickCurrent == 0){
+		return 0;
+	}
+	int polarity = joystickCurrent/abs(joystickCurrent);
+	long newInside = maxSpeed((abs(joystickCurrent)-127)*maxSpeed)/(deadzone-127);
+	return polarity*newInside;
+}
